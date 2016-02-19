4979a3cb1fb9fb709ee8244810056c7f5c7c7522
MengjiaoZhang <zmjlyn@gmail.com>
2016-02-18 19:14:51 -0500

    modify

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var $id = function(o){ return document.getElementById(o) || o};
+var $id = function(o){ return document.getElementById(o) || o;};
 $id("more_btn").onclick=function(){	
         height = Math.floor(Math.random()*200 + 200);
-        $id("wrap").innerHTML += '<li><div class="boxCont" style="height:' + height + 'px;"></div></li>';	
-}
-
+        $id("wrap").innerHTML += '<li><div class="boxCont" style="height:' + height + 'px;"></div></li>';
+};
 
 var parent = document.getElementById("skillNode");
 //var child = document.getElementById("s1");
