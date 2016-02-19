537f2505ad586d94cb4e3b3afa54b6927a0887c5
MengjiaoZhang <zmjlyn@gmail.com>
2016-02-18 19:56:06 -0500

    changeUrl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,12 +32,12 @@ public class Main {
 //          return "E=mc^2: " + energy + " = " + m.toString();
 //        });
 
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.html");
-        }, new FreeMarkerEngine());
+//    get("/", (request, response) -> {
+//            Map<String, Object> attributes = new HashMap<>();
+//            attributes.put("message", "Hello World!");
+//
+//            return new ModelAndView(attributes, "index.html");
+//        }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
