2f347de8f5366cde1d8107d5c7a94e12f4d80847
mentosforgithub <mentos.vicky@gmail.com>
2016-04-23 01:35:19 -0400

    423-2
    423

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import com.heroku.sdk.jdbc.DatabaseUrl;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -6,12 +5,13 @@ import java.util.Map;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import com.google.gson.Gson;
-import org.json.JSONObject;
+
 import static spark.Spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
 
@@ -19,30 +19,30 @@ public class Main {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
-    
 
+    get("/hello", (req, res) -> "Hello World");
 
-   
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
       try {
-        connection = getConnection();
+        connection = DatabaseUrl.extract().getConnection();
 
         Statement stmt = connection.createStatement();
-
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Usr (id int,name varchar(50),email varchar(100),passwd varchar(50))");
-        //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS moodblog(id int,name varchar(50),year int,month int,day int,weather varchar(500),location varchar(100),event varchar(500),withWho varchar(500),mood varchar(500))");
-        
-        ResultSet rs = stmt.executeQuery("SELECT * FROM moodblog");
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
         ArrayList<String> output = new ArrayList<String>();
         while (rs.next()) {
-          output.add( "Read from DB: " + rs.getString("name"));
-          output.add( "Read from DB: " + rs.getString("mood"));
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
         }
 
         attributes.put("results", output);
@@ -53,22 +53,8 @@ public class Main {
       } finally {
         if (connection != null) try{connection.close();} catch(SQLException e){}
       }
-  }, new FreeMarkerEngine());
+    }, new FreeMarkerEngine());
 
-  }
-
-  private static Connection getConnection() throws URISyntaxException, SQLException {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-    int port = dbUri.getPort();
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
-
-    if (dbUri.getUserInfo() != null) {
-      String username = dbUri.getUserInfo().split(":")[0];
-      String password = dbUri.getUserInfo().split(":")[1];
-      return DriverManager.getConnection(dbUrl, username, password);
-    } else {
-      return DriverManager.getConnection(dbUrl);
-    }
   }
 
 }
