dad5d31c62d4e5926c6ef1c1475622c432c3aa1d
FengxiLiu <mentos.vicky@gmail.com>
2016-04-27 18:45:20 -0400

    JSON POST add users

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,7 +28,29 @@ public class Main {
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());**/
-
+post("/signin", (req,res)->{
+        Connection con = null;
+        try{
+            con = DatabaseUrl.extract().getConnection();
+            JSONObject user = new JSONObject(req.body());//Use JSON to convey the message in signin
+            String username = user.getString("username");
+            String password = user.getString("password");
+            
+            String sql = "INSERT INTO Users (username,password) VALUES('"+username+"','"+password+"') ";
+            con = DatabaseUrl.extract().getConnection();
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Users");
+            stmt.executeUpdate(sql);
+            
+            return req.body();
+            
+        }catch(Exception e){
+            return e.getMessage();
+        }finally{
+            
+        }
+    });
+    
 get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
@@ -36,15 +58,14 @@ get("/db", (req, res) -> {
         connection = DatabaseUrl.extract().getConnection();
 
         Statement stmt = connection.createStatement();
-	stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Login (id int, username varchar(50),password varchar(50)");
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Comments_cmu (id int, username varchar(50),comments varchar(500)");
+	stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Login (username varchar(50),password varchar(50)");
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Comments_cmu (username varchar(50),comments varchar(500)");
         //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
 		
         ResultSet rs = stmt.executeQuery("SELECT * FROM Comments_cmu");
 
         ArrayList<String> output = new ArrayList<String>();
         while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("id"));
 		  output.add( "Read from DB: " + rs.getTimestamp("username"));
 		  output.add( "Read from DB: " + rs.getTimestamp("comments"));
         }
