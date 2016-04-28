58484b4ea53f31c1fb546324476269b305df109b
mentosforgithub <mentos.vicky@gmail.com>
2016-04-28 12:00:16 -0400

    add comments_Nicky java

---
+++ src/main/java/Comments_Nicky.java
@@ -0,0 +1,26 @@
+public class Comments_Nicky{
+	private String User;
+	private Date date;
+	private String Time;
+	private String Comment;
+	
+	public String getUser(){
+		return User;
+	}
+	public void setUser(String User){
+		this.User = User;
+	}
+	public String getTime(){
+		return Time;
+	}
+	public void setTime(){
+		date = new Date();
+		Time = date.toString();
+	}
+	public String getComment(){
+		return Comment;
+	}
+	public void setComment(String Comment){
+		this.Comment = Comment;
+	}
+}  
