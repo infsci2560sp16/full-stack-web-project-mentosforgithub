15cb354483b52674a5527584849e5e7bf8e91c08
mentosforgithub <mentos.vicky@gmail.com>
2016-04-27 23:28:29 -0400

    login verify js

---
+++ src/main/resources/public/javascript/login.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#signin').click(function()
+  {
+        var username = document.getElementById("login_username").value;
+        var password = document.getElementById("login_password").value;
+		
+        var send= JSON.stringify({ "login_username": username , "login_password":password});
+      $.ajax({
+          contentType:'application/json',
+          url:"/login",
+          type:"GET",
+          datatype: "json",
+          data: send,
+          success: function(data)
+          {
+            alert("succeeded");
+            window.location.href='/index.html';
+         }
+        });
+    }
+  );
+});
