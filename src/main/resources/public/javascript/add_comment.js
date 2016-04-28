e714522271aa565763cf30de9cefabddb4c8236e
mentosforgithub <mentos.vicky@gmail.com>
2016-04-28 12:42:58 -0400

    add comment js

---
+++ src/main/resources/public/javascript/add_comment.js
@@ -0,0 +1,20 @@
+function add_comment()
+  {
+        var username = document.getElementById("comment_cmu_username").value;
+        var comment = document.getElementById("comment_text").value;
+		var grade = document.getElementById("garde").value;
+		
+        var send= JSON.stringify({ "comment_cmu_username": username , "comment_text":comment,"grade":grade});
+      $.ajax({
+          contentType:'application/json',
+          url:"/addcomment",
+          type:"GET",
+          datatype: "json",
+          data: send,
+          success: function(data)
+          {
+            alert("succeeded");
+            window.location.href='/sight.html';
+         }
+        });
+    }
